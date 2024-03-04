50155d051928783ca987e0eed834b2f4a6cf5a21
Joey Gibson <joey@joeygibson.com>
2024-03-04 08:58:57 -0500

    macros 2

--- macros.lisp
+++ macros.lisp
@@ -4,3 +4,30 @@
 (defmacro my-unless (condition &rest body)
   `(if (not ,condition)
 	   (progn ,@body)))
+
+;; writing your own macros
+
+(defun prime-p (number)
+  (when (> number 1)
+	(loop for fac from 2 to (isqrt number) never (zerop (mod number fac)))))
+
+(defun next-prime (number)
+  (loop for n from number when (prime-p n) return n))
+
+(defmacro do-primes ((var start end) &body body)
+  (let ((ending-value-name (gensym)))
+   `(do ((,var (next-prime ,start) (next-prime (1+ ,var)))
+		 (,ending-value-name ,end))
+		((> ,var ,end))
+	  ,@body)))
+
+(defmacro with-gensyms ((&rest names) &body body)
+  `(let ,(loop for n in names collect `(,n (gensym)))
+	 ,@body))
+
+(defmacro do-primes-1 ((var start end) &body body)
+  (with-gensyms (ending-value-name)
+	`(do ((,var (next-prime ,start) (next-prime (1+ ,var)))
+		  (,ending-value-name ,end))
+		 ((> var ,end))
+	   ,@body)))
