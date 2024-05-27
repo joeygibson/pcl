c2481598e8bf27313b13b9bb086d1b9ff8450197
Joey Gibson <joey@joeygibson.com>
2024-05-27 12:53:35 -0400

    half of ch14

---
+++ files.lisp
@@ -0,0 +1,72 @@
+;; files and file IO
+
+(let ((in (open "./funcs.lisp")))
+  (format t "~a~%" (read-line in))
+  (close in))
+
+(let ((in (open "foo.lisp" :if-does-not-exist nil)))
+  (when in
+	(format t "~a~%" (read-line in))
+    (close in)))
+
+(let ((in (open "/some/file/name.txt" :if-does-not-exist nil)))
+  (when in
+    (loop for line = (read-line in nil)
+         while line do (format t "~a~%" line))
+    (close in)))
+
+; read will read a single sexp, ignoring whitespace and comments
+(let ((in (open "funcs.lisp")))
+  (format t "~a~%" (read in))
+  (close in)); reads a single sexp
+
+(let ((out (open "/tmp/dkdkdkkd.txt"
+				 :direction :output :if-exists :supersede)))
+  (close out))
+
+;; read-char, read-line, read, read-sequence
+;; write-char, write-line, write-string, write-sequence
+;; terpri - writes a newline
+;; fresh-line; only writes a newline if necessary
+
+;; print, prints an sexp, preceded by newline, followed by space
+;; prin1, just prints the sexp
+;; pprint, tried to print sexp in pleasing way
+;; princ, prints sexp in human-readable way
+;; write-byte to write to binary file
+
+(with-open-file (in "funcs.lisp")
+  (with-open-file (out "/tmp/funcs.lisp"
+					   :direction :output
+					   :if-exists :supersede)
+	(let ((line (read in)))
+	  (print line out))))
+
+;;; pathnames
+
+(pathname "/tmp/foo") ; #P"/tmp/foo"
+(pathname "./foo") ; #P"./foo"
+(pathname-directory (pathname "/tmp/foo")) ; (:absolute "tmp")
+(pathname-name (pathname "/tmp/foo")) ; "foo"
+(pathname-type (pathname "/tmp/foo")) ; nil
+(pathname-type (pathname "/tmp/foo.txt")) ; "txt
+
+(namestring (pathname "/tmp/foo")) ; "/tmp/foo"
+(directory-namestring (pathname "/tmp/foo")) "/tmp/"
+(file-namestring (pathname "/tmp/foo")) ; "foo"
+
+(make-pathname :device "c" :directory '(:absolute "foo" "bar")
+			   :name "baz")
+
+(make-pathname :type "html" :defaults (pathname "/tmp/foo"))
+
+(merge-pathnames #p"foo/bar.html" #p"html/") ; #p"html/foo/bar.html"
+
+(enough-namestring #p"/www/html/foo/bar.html" #p"/www/") ; "html/foo/bar.html"
+
+;; Interacting with the File System
+
+
+
+
+
