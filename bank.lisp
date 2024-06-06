06ee22236cc5ae8c5f19708f92f63be0048cbc1b
Joey Gibson <joey@joeygibson.com>
2024-06-06 19:06:49 -0400

    ch17 - partial

--- bank.lisp
+++ bank.lisp
@@ -1,24 +1,39 @@
-(defgeneric withdraw (account amount)
-  (:documentation "Withdraw the specified amount from account.
-Signal an error if the balance is less than amount."))
+(defclass bank-account ()
+  ((customer-name
+	:initarg :customer-name
+	:initform (error "Must specify a customer name"))
+   (account-number
+	:initform (random 999999999))
+   (balance
+	:initarg :balance
+	:initform 0)
+   account-type))
 
-(defmethod withdraw ((account bank-account) amount)
-  (when (< (balance account) amount)
-	(error "Account overdrawn."))
-  (decf (balance account) amount))
+(defparameter *account*
+  (make-instance 'bank-account :customer-name "John Doe"
+				 :balance 23.00))
 
-(defmethod withdraw ((account checking-account) amount)
-  (let ((overdraft (- amount (balance account))))
-	(when (plusp overdraft)
-	  (withdraw (overdraft-account account) overdraft)
-	  (incf (balance account) overdraft)))
-  (call-next-method))
+(defmethod initialize-instance :after ((account bank-account) &key)
+  (let ((balance (slot-value account 'balance)))
+	(setf (slot-value account 'account-type)
+		  (cond
+			((>= balance 100000) :gold)
+			((>= balance 50000) :silver)
+			(t :bronze)))))
 
-(defmethod withdraw ((account (eql *account-of-bank-president*)) amount)
-  (let ((overdraft (- amount (balance account))))
-	(when (plusp overdraft)
-	  (incf (balance account) (embezzle *bank* overdraft)))
-	(call-next-method)))
+;; initializer with &key arguments
+;; (defmethod initialize-instance :after ((account bank-account)
+;;                                        &key opening-bonus-percentage)
+;;   (when opening-bonus-percentage
+;;     (incf (slot-value account 'balance)
+;;           (* (slot-value account 'balance) (/ opening-bonus-percentage 100)))))
+
+(print (slot-value *account* 'customer-name))
+(print (slot-value *account* 'balance))
+(setf (slot-value *account* 'balance) 99.00)
+(print (slot-value *account* 'account-number))
+(print (slot-value *account* 'account-type))
+
 
 
 
